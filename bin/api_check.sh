7ae55391c90c61e516358f190b8b5b300eb015db
Tomas Ptacnik <t@tomor.cz>
2017-01-09 19:58:01 +0100

    Remove function for printing colorful output

--- bin/api_check.sh
+++ bin/api_check.sh
@@ -2,39 +2,7 @@
 # Basic "ping" script for web services
 # 2016-11-04
 
-# Note about color output and locale: If colors doesn't work, then it might be because of locale settings on the machine.
-# This scripts needs curl to return times in this type format "0.343".(e.g. en_US.UTF-8) 
-# When it returns the float number with comma "0,045" then the awk comparsion doesn't identify this as  
-# float and  comparsion doesn't work.  Use command "locale" to check locale settings on the PC
-
 # start recurrently via: watch -c -n2 ./api_check.sh
-
-#set -x # activate debugging from here
-
-# Function for printing colorfull curl result
-# It checks the response code and the time and make it colorful 
-# - if the code is not 200 or the time is too hight
-#
-# @param string Result of curl which looks like: "- ACCEPTANCE          | 200  | 1.188  |"
-# @return void  It echoes the incomming curl result
-function print_color {
-    local res="$1"
-    
-    # colorize is set to 1 when time is bigger then specified number 
-    code_fail=`echo $res | awk -F'|' '{ if ($2 != 200) { print "1" } else { print "0" } }'`
-    time_err=`echo $res | awk -F'|' '{ if ($3 > 3) { print "1" } else { print "0" } }'`
-    time_warn=`echo $res | awk -F'|' '{ if ($3 > 1.1) { print "1" } else { print "0" } }'`
-
-    if [ "$code_fail" -eq "1" ]; then
-      echo "$(tput setaf 1)$(tput rev)$res$(tput sgr 0)"
-    elif [ "$time_err" -eq "1" ]; then
-      echo "$(tput setaf 3)$(tput rev)$res$(tput sgr 0)"
-    elif [ "$time_warn" -eq "1" ]; then
-      echo "$(tput setaf 3)$res$(tput sgr 0)"
-    else
-      echo "$res"
-    fi
-}
 
 function influx_push {
     local res="$1"
@@ -43,17 +11,15 @@ function influx_push {
     http_code=`echo $res | awk -F'|' '{ print $2 }' | tr -d ' '`
     time_total=`echo $res | awk -F'|' '{ print $3 }' | tr -d ' '`
 
-#    echo $time_total
-curl -i -XPOST 'http://localhost:8086/write?db=mydb' --data-binary "http_services_response,host=${service_name} http_code=${http_code},time_total=${time_total}"
+    curl -i -XPOST 'http://localhost:8086/write?db=mydb' --data-binary "http_services_response,host=${service_name} http_code=${http_code},time_total=${time_total}"
 }
 
 function print_and_influx {
     local res="$1"
 
-    print_color "$res"
+    echo "$res"
     echo "$out_row"
     influx_push "$res"
-
 }
 
 out_head="   SERVICE NAME        | CODE | TIME   |"
